b784cd64b7758ab47429d3d5dbf4c70a62223383
narwhaldo <narwhaldo@gmail.com>
2014-07-18 16:43:15 -0700

    2.5 Real time updating of child_changed and child_removed

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,61 +8,87 @@
  * Controller of the ticTacTypeApp
  */
 angular.module('ticTacTypeApp')
-  .controller('MainCtrl', function ($scope, $timeout) {
-  	// $scope is a special object that makes
-	// its properties available to the view as
-	// variables. Here we set some default values:
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-    var rootRef = new Firebase('https://shining-fire-6606.firebaseio.com/');
-    var childRef = rootRef.child('message');
+    .controller('MainCtrl', function($scope, $timeout) {
+        var rootRef = new Firebase('https://tictactypeapp.firebaseio.com/');
+        var messagesRef = rootRef.child('messages');
+        // $scope is a special object that makes
+        // its properties available to the view as
+        // variables. Here we set some default values:
+        $scope.awesomeThings = [
+            'HTML5 Boilerplate',
+            'AngularJS',
+            'Karma'
+        ];
 
-    // Every time the value is update on the child reference, the on
-    // reference will fire
-    childRef.on('value', function(snapshot){
-        // We have to rap our function within a timeout so it goes up on the
-        // call stack
-        $timeout(function(){
-            snapshot.forEach(function(item){
-                console.log(item.name() + ' - ' +item.val() );
-                console.log(item.ref());
+        $scope.currentUser = null;
+        $scope.currentText = null;
+        $scope.messages = [];
+
+        // Every time the value is update on the child reference, the on
+        // reference will fire
+        messagesRef.on('child_added', function(snapshot) {
+            // We have to rap our function within a timeout so it goes up on the
+            // call stack
+            $timeout(function() {
+                var snapshotVal = snapshot.val();
+                console.log(snapshotVal);
+                $scope.messages.push({
+                    text: snapshotVal.text,
+                    user: snapshotVal.user,
+                    name: snapshot.name()
+                });
             });
-            var snapshotVal = snapshot.val();
-            console.log(snapshotVal); 
-            $scope.message = snapshotVal;
         });
-        
-    });
 
-    $scope.$watch('message.text', function(newVal){
-        if (!newVal){1
-            return;
+        messagesRef.on('child_changed', function(snapshot) {
+            // We have to rap our function within a timeout so it goes up on the
+            // call stack
+            $timeout(function() {
+                var snapshotVal = snapshot.val();
+                var message = findMessageByName(snapshot.name());
+                message.user = snapshotVal.user;
+                message.text = snapshotVal.text;
+            });
+        });
+
+        messagesRef.on('child_removed', function(snapshot) {
+            // We have to rap our function within a timeout so it goes up on the
+            // call stack
+            $timeout(function() {
+                deleteMessageByName(snapshot.name());
+            });
+        });
+
+        function deleteMessageByName(name){
+            for(var i = 0; i < $scope.messages.length; i++){
+                var currentMessage = $scope.messages[i];
+                if (currentMessage.name === name){
+                    $scope.messages.splice(i, 1);
+                    break;
+                }
+            }
         }
-        childRef.update({
-            text: newVal
-        })
+
+        function findMessageByName(name){
+            var messageFound = null;
+            for(var i = 0; i < $scope.messages.length; i++){
+                var currentMessage = $scope.messages[i];
+                if (currentMessage.name === name){
+                    messageFound = currentMessage;
+                    break;
+                }
+            }
+
+            return messageFound;
+        }
+
+        $scope.sendMessage = function(){
+            var newMessage = {
+                user : $scope.currentUser,
+                text : $scope.currentText
+            };
+            // Push gives us a unique name
+            messagesRef.push(newMessage);
+        };
+
     });
-
-    // CRUD operations
-    $scope.setMessage = function(){
-    	childRef.set({
-    		user: 'Bob',
-    		text: 'Hi'
-    	});
-    };
-
-    $scope.updateMessage = function(){
-    	childRef.update({
-    		lastname: 'Smith'
-    	});
-    };
-
-    $scope.deleteMessage = function(){
-    	childRef.remove();
-    };
-
-
-  });
